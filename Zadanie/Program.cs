952fa46af961e3cbf5af80014a252a06147ab290
SlIdErSoN <sliderson01@mail.ru>
2022-01-14 11:41:23 +0700

    ok

---
+++ Zadanie/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Chapter1.Task7;
+
+namespace Zadanie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Chapter1.Task7.Program.Main();
+        }
+    }
+}
